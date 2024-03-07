444cdaf6f183db9df0ebbc364699d95484e4de1f
Cupitacantikk <153527206+Cupitacantikk@users.noreply.github.com>
2024-03-07 22:06:41 +0700

    Update kasir.php

--- app/Views/dashboard/kasir.php
+++ app/Views/dashboard/kasir.php
@@ -1,1 +1,46 @@
-
+<div style="width: 100%; height: 100%; position: relative; background: #57DADA">
+    <div style="width: 1400px; height: 90px; left: 0px; top: 0px; position: absolute">
+        <div style="width: 1400px; height: 90px; left: 0px; top: 0px; position: absolute; background: #000957"></div>
+        <div style="width: 494.86px; height: 85px; left: 28.19px; top: 5px; position: absolute">
+            <div style="width: 438.47px; height: 85px; left: 56.39px; top: 0px; position: absolute; color: white; font-size: 48px; font-family: Mitr; font-weight: 400; word-wrap: break-word">Permata Laundry</div>
+            <div style="width: 44.72px; height: 60px; left: 0px; top: 10px; position: absolute">
+                <div style="width: 33.06px; height: 34px; left: 5.83px; top: 18px; position: absolute; border-radius: 9999px; border: 1px black solid"></div>
+                <div style="width: 44.72px; height: 60px; left: 0px; top: 0px; position: absolute; background: #000957; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 5px; border: 1px white solid"></div>
+                <div style="width: 32.08px; height: 32.78px; left: 6.32px; top: 18.50px; position: absolute; background: white; border: 1px white solid"></div>
+            </div>
+        </div>
+    </div>
+    <div style="width: 282px; height: 630px; left: 3px; top: 91px; position: absolute; background: #4FDD5D"></div>
+    <div style="width: 151px; height: 40.64px; left: 72px; top: 635.36px; position: absolute">
+        <div style="width: 151px; height: 40.64px; left: 0px; top: 40.64px; position: absolute; background: white; border-radius: 50px"></div>
+        <img style="width: 27.26px; height: 33.72px; left: 3.50px; top: 3.46px; position: absolute" src=<?= base_url("image/back_to.png")?>>
+        <div style="width: 134.92px; height: 40.64px; left: 16.08px; top: 0px; position: absolute; text-align: center; color: #5C9AE9; font-size: 21px; font-family: Rounded Mplus 1c; font-weight: 500; word-wrap: break-word">Back</div>
+    </div>
+    <div style="width: 65px; height: 67px; left: 105px; top: 114px; position: absolute;  border-radius: 9999px"></div>
+    <div style="width: 89px; height: 17px; left: 92px; top: 193px; position: absolute; color: white; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">CUPITA SARI</div>
+    <div style="width: 76px; height: 14px; left: 101px; top: 213px; position: absolute; color: white; font-size: 11px; font-family: Inter; font-weight: 400; word-wrap: break-word">Administrator</div>
+    <div style="width: 177px; height: 23px; left: 54px; top: 264px; position: absolute; background: #D9D9D9"></div>
+    <div style="width: 177px; height: 23px; left: 54px; top: 294px; position: absolute; background: #D9D9D9"></div>
+    <div style="width: 177px; height: 22px; left: 54px; top: 326px; position: absolute; background: #D9D9D9"></div>
+    <div style="width: 105px; height: 20px; left: 88px; top: 386px; position: absolute; background: #3FF5F5"></div>
+    <div style="width: 80px; height: 18px; left: 65px; top: 297px; position: absolute; color: #45A6A1; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">Profil</div>
+    <div style="width: 93px; height: 14px; left: 65px; top: 329px; position: absolute; color: #45A6A1; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">Paket</div>
+    <div style="width: 92p x; height: auto; left: 111px; top: 387px; position: absolute; color: #45A6A1; font-size: 14px; font-family: Inter; font-weight: 800; word-wrap: break-word"><a href="<?= base_url('logout') ?>">Logout</a></div>
+    <div style="width: 80px; height: 15px; left: 65px; top: 266px; position: absolute; color: #45A6A1; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">Dashboard</div>
+    <div style="width: 258px; height: 43px; left: 337px; top: 153px; position: absolute; color: black; font-size: 28px; font-family: Sansita One; font-weight: 400; word-wrap: break-word">DASHBOARD</div>
+    <div style="width: 1004px; height: 45px; left: 328px; top: 205px; position: absolute; background: #95D36F"></div>
+    <div style="width: 1004px; height: 45px; left: 328px; top: 261px; position: absolute; background: #5FA8EA"></div>
+    <div style="width: 626px; height: 33px; left: 356px; top: 211px; position: absolute"><span style="color: black; font-size: 20px; font-family: Sanchez; font-weight: 400; word-wrap: break-word">Selamat Datang </span><span style="color: black; font-size: 20px; font-family: Sanchez; font-style: italic; font-weight: 400; word-wrap: break-word">Admin</span><span style="color: black; font-size: 20px; font-family: Sanchez; font-weight: 400; word-wrap: break-word">, selamat bertugas</span></div>
+    <div style="width: 626px; height: 33px; left: 356px; top: 265px; position: absolute; color: black; font-size: 20px; font-family: Sarabun; font-weight: 400; word-wrap: break-word">Ringkasan Sistem</div>
+    <img style="width: 267px; height: 241px; left: 328px; top: 341px; position: absolute; background: linear-gradient(0deg, #C4C4C4 0%, #C4C4C4 100%); border-radius: 30px" src=<?= base_url("image/order_map.png") ?>>
+    <img style="width: 85px; height: 82px; left: 1094px; top: 341px; position: absolute"src=<?=base_url("image/menu5.png")?>>
+    <img style="width: 85px; height: 82px; left: 867px; top: 500px; position: absolute" src=<?=base_url("image/menu4.png")?>>
+    <img style="width: 85px; height: 82px; left: 635px; top: 500px; position: absolute" src=<?=base_url("image/menu2.png")?>>
+    <img style="width: 85px; height: 82px; left: 867px; top: 341px; position: absolute" src=<?=base_url("image/menu3.png")?>>
+    <img style="width: 84px; height: 82px; left: 635px; top: 341px; position: absolute" src=<?=base_url("image/menu1.png")?>>
+    <div style="width: 55px; height: 41px; left: 737px; top: 345px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">OUTLET<br/><br/>3</div>
+    <div style="width: 76px; height: 41px; left: 737px; top: 502px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">PELANGGAN<br/><br/>3</div>
+    <div style="width: 69px; height: 41px; left: 976px; top: 343px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">USER<br/><br/>3</div>
+    <div style="width: 107px; height: 41px; left: 976px; top: 502px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">TOTAL TRANSAKSI<br/><br/>12</div>
+    <div style="width: 55px; height: 41px; left: 1203px; top: 345px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">PAKET<br/><br/>3</div>
+</div>
