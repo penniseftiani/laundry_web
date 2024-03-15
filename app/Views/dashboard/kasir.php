37915f5a3560b4223a8c23e646240565d9e6ce46
Cupitacantikk <153527206+Cupitacantikk@users.noreply.github.com>
2024-03-15 12:43:58 +0700

    Update kasir.php

--- app/Views/dashboard/kasir.php
+++ app/Views/dashboard/kasir.php
@@ -1,46 +1,114 @@
-<div style="width: 100%; height: 100%; position: relative; background: #57DADA">
-    <div style="width: 1400px; height: 90px; left: 0px; top: 0px; position: absolute">
-        <div style="width: 1400px; height: 90px; left: 0px; top: 0px; position: absolute; background: #000957"></div>
-        <div style="width: 494.86px; height: 85px; left: 28.19px; top: 5px; position: absolute">
-            <div style="width: 438.47px; height: 85px; left: 56.39px; top: 0px; position: absolute; color: white; font-size: 48px; font-family: Mitr; font-weight: 400; word-wrap: break-word">Permata Laundry</div>
-            <div style="width: 44.72px; height: 60px; left: 0px; top: 10px; position: absolute">
-                <div style="width: 33.06px; height: 34px; left: 5.83px; top: 18px; position: absolute; border-radius: 9999px; border: 1px black solid"></div>
-                <div style="width: 44.72px; height: 60px; left: 0px; top: 0px; position: absolute; background: #000957; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 5px; border: 1px white solid"></div>
-                <div style="width: 32.08px; height: 32.78px; left: 6.32px; top: 18.50px; position: absolute; background: white; border: 1px white solid"></div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard Kasir</title>
+    <link rel="stylesheet" href="<?= base_url('assets/css/b.css') ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+</head>
+<body class="body-admin">
+    <div class="sidebar">
+        <div class="logo"></div>
+        <ul class="menu">
+            <li class="active">
+                <a href="">
+                    <i class="fas fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-chart-bar"></i>
+                    <span>Kelola Transaksi</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-dollar-sign"></i>
+                    <span>Kelola Pembayaran</span>
+                </a>
+            </li>
+            <li class="logout">
+                <a href="<?= base_url('logout') ?>">
+                    <i class="fas fa-sign-out"></i>
+                    <span>Logout</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="main--content">
+        <div class="header--wrapper">
+            <div class="header--title">
+                <span><h2>Dashboard<h2></span>
+                <h4>Kasir</h4>
+            </div>
+            <div class="user--info">
+                <div class="searh--box">
+                <i class="fa-solid fa-search"></i>
+                <input type="text" placeholder="Serach"/>
+                </div>
+                <img src="<?= base_url('image/cupita.jpg') ?>" alt=""/>
             </div>
         </div>
-    </div>
-    <div style="width: 282px; height: 630px; left: 3px; top: 91px; position: absolute; background: #4FDD5D"></div>
-    <div style="width: 151px; height: 40.64px; left: 72px; top: 635.36px; position: absolute">
-        <div style="width: 151px; height: 40.64px; left: 0px; top: 40.64px; position: absolute; background: white; border-radius: 50px"></div>
-        <img style="width: 27.26px; height: 33.72px; left: 3.50px; top: 3.46px; position: absolute" src=<?= base_url("image/back_to.png")?>>
-        <div style="width: 134.92px; height: 40.64px; left: 16.08px; top: 0px; position: absolute; text-align: center; color: #5C9AE9; font-size: 21px; font-family: Rounded Mplus 1c; font-weight: 500; word-wrap: break-word">Back</div>
-    </div>
-    <div style="width: 65px; height: 67px; left: 105px; top: 114px; position: absolute;  border-radius: 9999px"></div>
-    <div style="width: 89px; height: 17px; left: 92px; top: 193px; position: absolute; color: white; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">CUPITA SARI</div>
-    <div style="width: 76px; height: 14px; left: 101px; top: 213px; position: absolute; color: white; font-size: 11px; font-family: Inter; font-weight: 400; word-wrap: break-word">Administrator</div>
-    <div style="width: 177px; height: 23px; left: 54px; top: 264px; position: absolute; background: #D9D9D9"></div>
-    <div style="width: 177px; height: 23px; left: 54px; top: 294px; position: absolute; background: #D9D9D9"></div>
-    <div style="width: 177px; height: 22px; left: 54px; top: 326px; position: absolute; background: #D9D9D9"></div>
-    <div style="width: 105px; height: 20px; left: 88px; top: 386px; position: absolute; background: #3FF5F5"></div>
-    <div style="width: 80px; height: 18px; left: 65px; top: 297px; position: absolute; color: #45A6A1; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">Profil</div>
-    <div style="width: 93px; height: 14px; left: 65px; top: 329px; position: absolute; color: #45A6A1; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">Paket</div>
-    <div style="width: 92p x; height: auto; left: 111px; top: 387px; position: absolute; color: #45A6A1; font-size: 14px; font-family: Inter; font-weight: 800; word-wrap: break-word"><a href="<?= base_url('logout') ?>">Logout</a></div>
-    <div style="width: 80px; height: 15px; left: 65px; top: 266px; position: absolute; color: #45A6A1; font-size: 14px; font-family: Inter; font-weight: 400; word-wrap: break-word">Dashboard</div>
-    <div style="width: 258px; height: 43px; left: 337px; top: 153px; position: absolute; color: black; font-size: 28px; font-family: Sansita One; font-weight: 400; word-wrap: break-word">DASHBOARD</div>
-    <div style="width: 1004px; height: 45px; left: 328px; top: 205px; position: absolute; background: #95D36F"></div>
-    <div style="width: 1004px; height: 45px; left: 328px; top: 261px; position: absolute; background: #5FA8EA"></div>
-    <div style="width: 626px; height: 33px; left: 356px; top: 211px; position: absolute"><span style="color: black; font-size: 20px; font-family: Sanchez; font-weight: 400; word-wrap: break-word">Selamat Datang </span><span style="color: black; font-size: 20px; font-family: Sanchez; font-style: italic; font-weight: 400; word-wrap: break-word">Admin</span><span style="color: black; font-size: 20px; font-family: Sanchez; font-weight: 400; word-wrap: break-word">, selamat bertugas</span></div>
-    <div style="width: 626px; height: 33px; left: 356px; top: 265px; position: absolute; color: black; font-size: 20px; font-family: Sarabun; font-weight: 400; word-wrap: break-word">Ringkasan Sistem</div>
-    <img style="width: 267px; height: 241px; left: 328px; top: 341px; position: absolute; background: linear-gradient(0deg, #C4C4C4 0%, #C4C4C4 100%); border-radius: 30px" src=<?= base_url("image/order_map.png") ?>>
-    <img style="width: 85px; height: 82px; left: 1094px; top: 341px; position: absolute"src=<?=base_url("image/menu5.png")?>>
-    <img style="width: 85px; height: 82px; left: 867px; top: 500px; position: absolute" src=<?=base_url("image/menu4.png")?>>
-    <img style="width: 85px; height: 82px; left: 635px; top: 500px; position: absolute" src=<?=base_url("image/menu2.png")?>>
-    <img style="width: 85px; height: 82px; left: 867px; top: 341px; position: absolute" src=<?=base_url("image/menu3.png")?>>
-    <img style="width: 84px; height: 82px; left: 635px; top: 341px; position: absolute" src=<?=base_url("image/menu1.png")?>>
-    <div style="width: 55px; height: 41px; left: 737px; top: 345px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">OUTLET<br/><br/>3</div>
-    <div style="width: 76px; height: 41px; left: 737px; top: 502px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">PELANGGAN<br/><br/>3</div>
-    <div style="width: 69px; height: 41px; left: 976px; top: 343px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">USER<br/><br/>3</div>
-    <div style="width: 107px; height: 41px; left: 976px; top: 502px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">TOTAL TRANSAKSI<br/><br/>12</div>
-    <div style="width: 55px; height: 41px; left: 1203px; top: 345px; position: absolute; color: black; font-size: 12px; font-family: Signika; font-weight: 400; word-wrap: break-word">PAKET<br/><br/>3</div>
-</div>
+
+        <div class="card--container">
+            <h3 class="main--title">Data Laundry</h3>
+            <div class="card--wrapper">
+                <div class="payment--card light-red">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">
+                                Detail Transaksi
+                            </span>
+                            <span class="amount--value">Rp500.00</span>
+                        </div>
+                        <i class="fas fa-rupiah-sign icon"></i>
+                    </div>
+                    <span class="card-detail">****</span>
+                </div>
+                <div class="payment--card light-purple">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">
+                                Jumlah Order Cucian
+                            </span>
+                            <span class="amount--value">20 Prs</span>
+                        </div>
+                        <i class="fas fa-list icon dark-purple" ></i>
+                    </div>
+                    <span class="card-detail">****</span>
+                </div>
+                <div class="payment--card light-blue">
+                    <div class="card--header">
+                        <div class="amount">
+                            <span class="title">
+                                Status Cucian
+                            </span>
+                            <span class="amount--value">Selesai</span>
+                        </div>
+                        <i class="fas fa-check icon dark-blue"></i>
+                    </div>
+                    <span class="card-detail">****</span>
+                </div>
+                </div>
+            </div>
+            <div class="tabular--wrapper">
+                <h3 class="main--title">Ringkasan Data Transaksi</h3>
+                <div class="table--container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Tanggal</th>
+                                <th>Kode Invoice</th>
+                                <th>Nama Pelanggan</th>
+                                <th>Jenis Paket</th>
+                                <th>Harga</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        
+</body>
+</html>
