839938513a749e604d9f5f6f897820066cd490a6
penni seftiani <penni.seftiani@gmail.com>
2024-03-16 13:09:41 +0700

    update view login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Login</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url("assets/dist/css/style.css"); ?>">
 </head>
-<body  class="body-login">
-      <div class="login-container">
+
+<body class="body-login">
+    <div class="login-container">
         <div class="login">
             <form action='login/auth' method="post" style="border-sizing: border-box">
-                <h1>Login</h1><hr>
+                <h1>Login</h1>
+                <hr>
                 <p>Halaman Login</p>
                 <label for="">Username</label>
-                <input type="text" id="username" name="username"required placeholder="Username">
+                <input type="text" id="username" name="username" required placeholder="Username">
                 <label for="">Password</label>
-                <input type="text" id="password" name="password"required placeholder="Password">
+                <input type="text" id="password" name="password" required placeholder="Password">
                 <button>Login</button>
                 <p>
                     <a href="">Forgot password ?</a>
@@ -23,8 +26,9 @@
             </form>
         </div>
         <div class="right">
-        <img src="image/order_map.png" alt="">
+            <img src="<?= base_url("assets/dist/img/order_map.png"); ?> " alt="ordermap">
         </div>
     </div>
 </body>
+
 </html>
